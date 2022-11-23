efffa283a57fca80002eb44ce1f64f93552a6692
usama <usamaali052@gmail.com>
2022-11-23 21:53:25 +0500

    update user status completed

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const { body } = require('express-validator')
 const router = express.Router()
 
 const authController = require('../controllers/auth')
+const isAuth = require('../middleware/is-auth')
 const User = require('../models/user')
 
 router.put('/signup', [
@@ -29,5 +30,14 @@ router.put('/signup', [
 )
 
 router.post('/login', authController.login)
+
+router.get('/status', isAuth, authController.getUserStatus)
+
+router.patch('/status', isAuth, [
+  body('status')
+    .trim()
+    .not()
+    .isEmpty()
+], authController.updateUserStatus)
 
 module.exports = router
